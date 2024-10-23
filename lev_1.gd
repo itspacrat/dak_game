b591dfb8d28233d77621650f0b0df20a43bfa956
itspacrat <blakemichaelgaynor@gmail.com>
2024-10-23 14:52:18 -0700

    clean up scaffold comments

--- lev_1.gd
+++ lev_1.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-# on load
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+	#var sc_guy = load("res://guy.tscn")
+	#var bad_guy = load("res://badguy.tscn")
 
-# per frame
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
