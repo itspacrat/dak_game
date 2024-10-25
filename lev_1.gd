3ac1f8ad2a0948db29e2b4a1a0edc88b8d55a157
itspacrat <blakemichaelgaynor@gmail.com>
2024-10-25 09:04:25 -0700

    preload `badguy` tscn

--- lev_1.gd
+++ lev_1.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+var peon = preload("res://badguy.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	peon.instantiate()
 	pass
 	#var sc_guy = load("res://guy.tscn")
 	#var bad_guy = load("res://badguy.tscn")
